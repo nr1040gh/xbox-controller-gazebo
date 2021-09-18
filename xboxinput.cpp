d8dd82e32891d92c7889dbbd6ec2eb5f37b13013
Nicholas Richards <nicholasrichards97@gmail.com>
2021-09-18 14:05:28 -0400

    Added deadzone support for left and right joystick, bug fixes

--- xboxinput.cpp
+++ xboxinput.cpp
@@ -4,6 +4,7 @@
 #include <unistd.h> //for read() and write()
 #include <map>
 #include <string>
+#include <cmath>
 
 //https://stackoverflow.com/questions/16695432/input-event-structure-description-from-linux-input-h
 
@@ -34,6 +35,10 @@
 16:DPAD X
 17:DPAD Y
 */
+
+//To do: create class for buttons, add deadzone method
+    // Break into header and cpp files
+// To do: create testing file/program
 
 int main(){
     //https://www.kernel.org/doc/html/v5.3/input/joydev/joystick-api.html
@@ -73,13 +78,57 @@ int main(){
     struct input_event e; 
     int code;
     std::string input;
+
+    int count = 1, prev = 0;
     while (read(fd, &e, sizeof(e))){
         //if (e.type == EV_KEY){
             //std::cout << "type: KEY" << e.type << std::endl;
             
         //}
         code = e.code;
+        input = mapping[code];
 
+        //Apply dead zones for control sticks - If it's not outside deadzone, continue checking for buttons
+        if (code <= 2 || (code >= 3 && code <= 4))
+        {
+            int deadzone = 6000; //calibrated based on testing my controller only - move this further out of the function
+            switch (code)
+            {
+            case 0:
+                if (std::abs(e.value) > deadzone)
+                {
+                    std::cout << "type: JS" << e.type << std::endl;
+                    std::cout << "Input: " << input << std::endl;
+                    std::cout << "value: " << e.value << std::endl;
+                    std::cout << std::endl;
+                }
+                
+            case 1:
+                if (std::abs(e.value) > deadzone)
+                {
+                    std::cout << "type: JS" << e.type << std::endl;
+                    std::cout << "Input: " << input << std::endl;
+                    std::cout << "value: " << e.value << std::endl;
+                    std::cout << std::endl;
+                }
+            case 3:
+                if (std::abs(e.value) > deadzone)
+                {
+                    std::cout << "type: JS" << e.type << std::endl;
+                    std::cout << "Input: " << input << std::endl;
+                    std::cout << "value: " << e.value << std::endl;
+                    std::cout << std::endl;
+                }
+            case 4:
+                if (std::abs(e.value) > deadzone)
+                {
+                    std::cout << "type: JS" << e.type << std::endl;
+                    std::cout << "Input: " << input << std::endl;
+                    std::cout << "value: " << e.value << std::endl;
+                    std::cout << std::endl;
+                }
+            }
+        }
 
         //if ((e.type == EV_ABS) && (e.code == 0)){
         //   std::cout << "type: JS" << e.type << std::endl;
@@ -87,9 +136,16 @@ int main(){
         //   std::cout << "value: " << e.value << std::endl;
         //  std::cout << std::endl;
         //}
+        if (prev == code){
+            count ++;
+        }
+
+        else{
+            count = 1;
+        }
+
+        prev = code;
         
-        
-        input = mapping[code];
 
         switch (code){
             /*
@@ -103,11 +159,15 @@ int main(){
                 std::cout << "Input: " << input << std::endl;
                 std::cout << "value: " << e.value << std::endl;
                 std::cout << std::endl;
+            */
+
             case 2:
                 std::cout << "type: JS" << e.type << std::endl;
                 std::cout << "Input: " << input << std::endl;
                 std::cout << "value: " << e.value << std::endl;
                 std::cout << std::endl;
+
+            /*
             case 3:
                 std::cout << "type: JS" << e.type << std::endl;
                 std::cout << "Input: " << input << std::endl;
@@ -118,81 +178,96 @@ int main(){
                 std::cout << "Input: " << input << std::endl;
                 std::cout << "value: " << e.value << std::endl;
                 std::cout << std::endl;
+            */
             case 5:
                 std::cout << "type: JS" << e.type << std::endl;
                 std::cout << "Input: " << input << std::endl;
                 std::cout << "value: " << e.value << std::endl;
                 std::cout << std::endl;
-            */
+            
             case 16:
                 std::cout << "type: JS" << e.type << std::endl;
                 std::cout << "Input: " << input << std::endl;
                 std::cout << "value: " << e.value << std::endl;
+                std::cout<< "count: " << count << std::endl;  
                 std::cout << std::endl;
             case 17:
                 std::cout << "type: JS" << e.type << std::endl;
                 std::cout << "Input: " << input << std::endl;
                 std::cout << "value: " << e.value << std::endl;
+                std::cout<< "count: " << count << std::endl;  
                 std::cout << std::endl;
             case 304:
                 std::cout << "type: JS" << e.type << std::endl;
                 std::cout << "Input: " << input << std::endl;
                 std::cout << "value: " << e.value << std::endl;
+                std::cout<< "count: " << count << std::endl;  
                 std::cout << std::endl;
             case 305:
                 std::cout << "type: JS" << e.type << std::endl;
                 std::cout << "Input: " << input << std::endl;
                 std::cout << "value: " << e.value << std::endl;
+                std::cout<< "count: " << count << std::endl;  
                 std::cout << std::endl;
             case 307:
                 std::cout << "type: JS" << e.type << std::endl;
                 std::cout << "Input: " << input << std::endl;
                 std::cout << "value: " << e.value << std::endl;
+                std::cout<< "count: " << count << std::endl;  
                 std::cout << std::endl;
             case 308:
                 std::cout << "type: JS" << e.type << std::endl;
                 std::cout << "Input: " << input << std::endl;
                 std::cout << "value: " << e.value << std::endl;
+                std::cout<< "count: " << count << std::endl;  
                 std::cout << std::endl;
             case 314:
                 std::cout << "type: JS" << e.type << std::endl;
                 std::cout << "Input: " << input << std::endl;
                 std::cout << "value: " << e.value << std::endl;
+                std::cout<< "count: " << count << std::endl;  
                 std::cout << std::endl;
             case 315:
                 std::cout << "type: JS" << e.type << std::endl;
                 std::cout << "Input: " << input << std::endl;
                 std::cout << "value: " << e.value << std::endl;
+                std::cout<< "count: " << count << std::endl;  
                 std::cout << std::endl;
             case 310:
                 std::cout << "type: JS" << e.type << std::endl;
                 std::cout << "Input: " << input << std::endl;
                 std::cout << "value: " << e.value << std::endl;
+                std::cout<< "count: " << count << std::endl;  
                 std::cout << std::endl;
             case 311:
                 std::cout << "type: JS" << e.type << std::endl;
                 std::cout << "Input: " << input << std::endl;
                 std::cout << "value: " << e.value << std::endl;
+                std::cout<< "count: " << count << std::endl;  
                 std::cout << std::endl;
             case 316:
                 std::cout << "type: JS" << e.type << std::endl;
                 std::cout << "Input: " << input << std::endl;
                 std::cout << "value: " << e.value << std::endl;
+                std::cout<< "count: " << count << std::endl;  
                 std::cout << std::endl;
             case 317:
                 std::cout << "type: JS" << e.type << std::endl;
                 std::cout << "Input: " << input << std::endl;
                 std::cout << "value: " << e.value << std::endl;
+                std::cout<< "count: " << count << std::endl;  
                 std::cout << std::endl;
             case 318:
                 std::cout << "type: JS" << e.type << std::endl;
                 std::cout << "Input: " << input << std::endl;
                 std::cout << "value: " << e.value << std::endl;
+                std::cout<< "count: " << count << std::endl;  
                 std::cout << std::endl;
-
-            
+    
         }
-        
+
+              
+
         
 
         
